1c85e9eb04663519837a06f906db5f5a62d18e26
RAHUL SAGAR PABBA <100937627+rahulsagar500@users.noreply.github.com>
2023-01-21 23:03:52 +0530

    Insertion sort in C++ simple code
    I tried to write the code in as much as simply as i can so that even the code is understandable for the people who just started coding.

---
+++ Array/Insertionsort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+     while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {9,8,7,6,5,4,3,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    insertionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
